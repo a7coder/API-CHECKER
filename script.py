a9046f26b2eab48af6aabd8db0e5a925d20c040b
A7Coder <a7coder@gmail.com>
2023-06-10 19:19:08 +0530

    V4

--- script.py
+++ script.py
@@ -7,11 +7,14 @@ import csv
 import socket
 import ssl
 from collections import deque
+import time
 
-def get_domain(url):
+def get_domain(url:str):
     Domain_name = ''
+   
     x = url.split("/")
     
+
     if(x[0] == "https:" or x[0] == "http:"):
         x = x[2].split(".")
     else:
@@ -25,7 +28,7 @@ def get_domain(url):
 
 def check_port(port, url):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)  # Set the timeout to 1 second
+    sock.settimeout(1)  
     result = sock.connect_ex((url, port))
     if result == 0:
         service_name = socket.getservbyport(port)
@@ -51,9 +54,8 @@ def check_open_ports(url: str):
     with ThreadPoolExecutor(max_workers=50) as executor:
         
         tasks = [executor.submit(check_port, port, url)
-                 for port in range(1, 500)]
+                 for port in range(1, 65535)]
 
-      
         for future in concurrent.futures.as_completed(tasks):
             result = future.result()
             if result:
@@ -64,38 +66,45 @@ def check_open_ports(url: str):
 
 def check_ssl_upgrade(url: str,open_port_list:list):
 
-    if (443, 'https') in open_port_list:
-        print("************************ Checking the TLS Certificate, Please Wait *********************")
-        print()
+    try:
 
-        if url.startswith("http://"):
-            url = url[len("http://"):]
-        elif url.startswith("https://"):
-            url = url[len("https://"):]
+        if (443, 'https') in open_port_list:
+            print("************************ Checking the TLS Certificate, Please Wait *********************")
+            print()
 
-        if url.endswith('/'):
-            url = url[:-1]
+            if url.startswith("http://"):
+                url = url[len("http://"):]
+            elif url.startswith("https://"):
+                url = url[len("https://"):]
 
-        context =  context = ssl.create_default_context()
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if url.endswith('/'):
+                url = url[:-1]
+
+            context =  context = ssl.create_default_context()
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            
+            sslSocket = context.wrap_socket(s, server_hostname = url)
+            sslSocket.connect((url, 443))
+            tls_version=sslSocket.version()
         
-        sslSocket = context.wrap_socket(s, server_hostname = url)
-        sslSocket.connect((url, 443))
-        tls_version=sslSocket.version()
-    
-        
-        sslSocket.close()
-        if 'TLSv1.3' == tls_version:
-            print("************************ No Need to Upgrade TLS Certificate *********************")
+            
+            sslSocket.close()
+            if 'TLSv1.3' == tls_version:
+                print("************************ No Need to Upgrade TLS Certificate *********************")
+                
+            else:
+                print("************************ Please Upgrade TLS Certificate *********************")
             
         else:
-            print("************************ Please Upgrade TLS Certificate *********************")
-           
-    else:
-        print("************************ Port 443 is not Open *********************")
+            print("************************ Port 443 is not Open *********************")
 
-    print()
-    print(f'************************ Your Current TLS Version is {tls_version} *********************')
+        print()
+        print(f'************************ Your Current TLS Version is {tls_version} *********************')
+        print()
+    
+    except:
+        print("************************ SSL/TLS Certificate is not Present *********************")
+        print()
         
 
 
@@ -135,52 +144,61 @@ def get_all_links(url):
 
     all_links.add(url)
     queue_links.append(url)
-
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
     
     while queue_links:
 
         url= queue_links.popleft()
         
+        if url.endswith('/'):
+            url=url[:-1]
         if url not in visited and get_domain(url) == actual_domain:
             
-            response = requests.get(url, headers=headers)
+            response = requests.get(url)
+            if not response.status_code == 200:
+                all_links.add(url)
+                visited.add(url)
+                continue
+                
             soup = BeautifulSoup(response.text, 'html.parser')
 
             for anchor in soup.find_all('a'):
 
                 href = anchor.get('href')
 
-                if href and not (href.startswith("mailto:") or href.startswith("javascript:") or href.startswith('tel') or href.startswith('#')):
+                if href and not (href.startswith("mailto:") or href.startswith("javascript:") or href.startswith('tel') or href.startswith('/#') or href.startswith('#')):
 
                     absolute_url = href
 
                     if not (absolute_url.startswith("http") or absolute_url.startswith("https")) :
 
                         absolute_url =urljoin(url, href)                 
-
+                     
+                    if absolute_url.endswith('/'):
+                        absolute_url=absolute_url[:-1]
+                    
                     if absolute_url not in parent_dict:
                         parent_dict[absolute_url] = set()
 
                     parent_dict[absolute_url].add(url)
                     all_links.add(absolute_url)
+                    
                     queue_links.append(absolute_url)
 
         visited.add(url)
 
     print("********************** All Links Are Fetched **********************")
-    print(all_links)
+    print()
     return all_links
    
 
 def check_link_status(link):
     try:
-        response = requests.head(link)
-        return response.status_code
-    except requests.exceptions.RequestException:
-        return None
-
-
+        response = requests.get(link,timeout=2)
+        if response.status_code !=200:
+            return False 
+        return True
+    except :
+        return False
 
 def scan_website(url):
     try:    
@@ -200,7 +218,7 @@ def scan_website(url):
                 link = futures[future]
                 status_code = future.result()
 
-                if status_code is not None:
+                if status_code:
                     valid_links += 1
 
                 else:
@@ -222,23 +240,23 @@ def save_results(url, total_links, valid_links, broken_links, open_ports_list, s
             writer.writerow(["Website URL", "Total Links",
                             "Valid Links", "Broken Links"])
             writer.writerow([url, total_links, valid_links, len(broken_links)])
-            writer.writerow([])  # Empty row
+            writer.writerow([])   
 
             file.write("Open Ports")
-            writer.writerow([])  # Empty row
+            writer.writerow([])   
 
             for port in open_ports_list:
                 file.write(str(port)+'\n')
 
-            writer.writerow([])  # Empty row
+            writer.writerow([])   
 
             file.write("Security Headers Present")
-            writer.writerow([])  # Empty row
+            writer.writerow([])   
 
             for header in security_headers_list:
                 file.write(str(header)+'\n')
 
-            writer.writerow([])  # Empty row
+            writer.writerow([])   
             if broken_links:
                 writer.writerow(["Broken Link", 'Found At'])
 
@@ -253,34 +271,44 @@ def save_results(url, total_links, valid_links, broken_links, open_ports_list, s
 
 def main():
     try:
+        
         print()
-        url = input("Enter the website URL: ")
+        url = input("Enter the website URL: ").strip()
         print()
   
         save_file = input(
             "Enter the file name to save results (leave blank for no saving): ")
         print()
 
-        if not url.startswith('http://') and not url.startswith('https://'):
+        t1=time.time()
+
+        if not (url.startswith('http://') or  url.startswith('https://')):
             url = 'http://' + url
+       
 
-        https_url = url.replace('http://', 'https://', 1)
-
-        if requests.head(https_url).status_code == 200:
-                
-                url = https_url
-
-        elif 'www' not in https_url:
-                    
-            https_url =https_url.replace('://', '://www.', 1)
-
-            if requests.head(https_url).status_code == 200:
-                
-                    url = https_url
+        try:
+            
+            r =requests.get(url,timeout=2)
+            
+            if r.status_code == 200:
+                    url = r.url 
+          
+        except:
+            try:
+                if 'www' not in url :
+                        x =url.replace('://', '://www.', 1)
+                        t= requests.get(x,timeout=2)
+                        if t.status_code == 200:
+                            url = t.url
+            except:
+                print('********************** Website is Down **********************')  
+                print()         
+                return
 
         print ('Checking the URL .... ', url)
         print()
         all_links, valid_links, broken_links = scan_website(url)
+        t2 = time.time()
         open_ports_list = check_open_ports(url)
         security_headers_list = check_security_headers(url)
 
@@ -297,19 +325,19 @@ def main():
         print()
         
         check_ssl_upgrade(url,open_ports_list)          
-
+        
         if broken_links:
             print('********************** List of Broken Links **********************')
-
+            print()
         for link in broken_links:
             print("Broken link: ", link, ' Found At: ', parent_dict[link])
 
         if save_file:
             save_results(url, len(all_links), valid_links,
-                         broken_links, open_ports_list, security_headers_list, save_file)
-            
+                         broken_links, open_ports_list, security_headers_list, save_file)           
         
-
+        print()
+        print('*************** Running Time ********************** ',t2-t1)
         print()
         print("********************** Script Completed **********************")
         print()
