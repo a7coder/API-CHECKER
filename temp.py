d8f7e49294c88eaaa28397e7748d786ddb7990ab
A7Coder <a7coder@gmail.com>
2023-06-10 19:19:23 +0530

    V4

--- temp.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-h={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
-print(requests.get(' http://www.linkedin.com/in/ayush-kothiyal',headers=h).status_code)
